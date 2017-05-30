39d2e023a7a1a22b7dbbd1708449d14ab6b205f9
Neil James Cook <neil.james.cook@gmail.com>
2017-05-30 17:23:34 -0400

    Added initialisation file for module

---
+++ __init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 30/05/17 at 5:21 PM
+
+@author: neil
+
+Program description here
+
+Version 0.0.0
+"""
+
+from . import VOtable_warning_fixer
+
+__author__ = "Neil Cook"
+__email__ = 'neil.james.cook@gmail.com'
+__version__ = '0.1'
+__all__ = ['Add_buttons', 'Rectangle_Selector']
+
+# =============================================================================
+#  Functions
+# =============================================================================
+VOfix = VOtable_warning_fixer.
